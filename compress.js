1d22de2b4a52cef1a13b3323bb1769de96d1d79f
jddelia <jddelia123@gmail.com>
2023-03-24 10:07:18 -0400

    update

--- compress.js
+++ compress.js
@@ -1,27 +1,3 @@
-// function compress(s) {
-//   let result = "";
-
-//   let currentString = "";
-//   for (let i = 0; i < s.length; i++) {
-//     if (
-//       currentString.length < 1 ||
-//       s[i] === currentString[currentString.length - 1]
-//     ) {
-//       currentString += s[i];
-//     }
-
-//     if (s[i] !== currentString[currentString.length - 1] || !s[i + 1]) {
-//       if (currentString.length === 1) {
-//         result += currentString[0];
-//       } else {
-//         result += currentString.length.toString() + currentString[0];
-//       }
-//       currentString = s[i];
-//     }
-//   }
-//   return result;
-// }
-
 function compress(s) {
   let result = "";
 
