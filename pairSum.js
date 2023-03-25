c19b939c7517ad4c66938bbb684f9a6af1b908eb
jddelia <jddelia123@gmail.com>
2023-03-25 17:50:02 -0400

    update

--- pairSum.js
+++ pairSum.js
@@ -25,11 +25,55 @@ function pairSum2(arr, target) {
   }
 }
 
+function pairSumRecursive(arr, target, startIndex = 0) {
+  if (startIndex >= arr.length - 1) {
+    return undefined;
+  }
+
+  let num = arr[startIndex];
+  for (let i = startIndex + 1; i < arr.length; i++) {
+    if (num + arr[i] === target) {
+      return [startIndex, i];
+    }
+  }
+
+  return pairSumRecursive(arr, target, startIndex + 1);
+}
+
+function pairSumRecursive2(arr, target, startIndex = 0, seenNums = {}) {
+  if (startIndex >= arr.length) {
+    return undefined;
+  }
+
+  let num = arr[startIndex];
+  let complement = target - num;
+  if (seenNums[complement] !== undefined) {
+    return [seenNums[complement], startIndex];
+  }
+
+  seenNums[num] = startIndex;
+  return pairSumRecursive2(arr, target, startIndex + 1, seenNums);
+}
+
+console.log("\nPair Sum\n");
+
 console.log(pairSum([1, 2, 3, 4, 5], 9));
 console.log(pairSum([3, 2, 5, 4, 1], 8));
 console.log(pairSum([4, 7, 9, 2, 5, 1], 5));
 
-console.log("\nPAIR SUM 2\n");
+console.log("\nPair Sum 2\n");
+
+console.log(pairSum2([1, 2, 3, 4, 5], 9));
+console.log(pairSum2([3, 2, 5, 4, 1], 8));
+console.log(pairSum2([4, 7, 9, 2, 5, 1], 5));
+
+console.log("\nPair Sum Recursive\n");
+
+console.log(pairSum([1, 2, 3, 4, 5], 9));
+console.log(pairSum([3, 2, 5, 4, 1], 8));
+console.log(pairSum([4, 7, 9, 2, 5, 1], 5));
+
+console.log("\nPair Sum Recursive 2\n");
 
 console.log(pairSum2([1, 2, 3, 4, 5], 9));
 console.log(pairSum2([3, 2, 5, 4, 1], 8));
